f9219b466f341593c058e0460a172e7e367af29a
Malcolm Gill <malcolm.gill@gmail.com>
2016-03-22 14:41:48 +1100

    Fix csrf code.
    Change-Id: I22a4739a222baed72aaed7e73af900e7776f58b7

--- reporting_view.py
+++ reporting_view.py
@@ -1,12 +1,52 @@
 #!/usr/bin/env python
 
 import os
+import time
 import hashlib
 import flask
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+
+class TimeoutException(Exception):
+    """Raised when reading a secret key times out.
+    Probably means that another thread has failed in the process of writing
+    the secret file.
+    """
+    pass
+
+def get_secret_key(secret_directory='/tmp/reporting_view', secret_file='secret.txt', timeout=5):
+    """Load secret key from specified path, if it exists, or randomly generate
+    one and save it at the path.
+
+    Keyword arguments:
+    secret_directory -- path to directory containing secret key, which will be
+                        created if it does not exist
+    secret_file -- file within secret_directory containing secret key, which
+                   will be created if it does not exist
+    timeout -- wait at most this many seconds for secret_file to appear in
+               secret_directory; raise TimeoutException on failure
+    """
+    path = os.path.join(secret_directory, secret_file)
+    try:
+        os.mkdir(secret_directory, 0700) # raises OSError if directory already exists
+        # since no OSError was raised, key is being generated for first time here
+        with open(path, 'w') as f:
+            key = os.urandom(24) # Flask quickstart suggests 24 random bytes makes an acceptable secret key
+            f.write(key)
+            return key
+    except OSError as e:
+        # secret_directory exists; assume somebody else is generating (or has generated) key
+        for timer in xrange(timeout):
+            try:
+                with open(path, 'r') as f:
+                    return f.read()
+            except IOError:
+                # secret file isn't (yet?) readable -- maybe someone else is in the process of creating it...
+                time.sleep(1)
+        raise TimeoutException("Couldn't read {path} after {timeout} seconds.".format(path=path, timeout=timeout))
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = get_secret_key()
 
 @app.route('/')
 def login():
@@ -40,7 +80,7 @@ def generate_csrf_token():
         session['_csrf_token'] = hashlib.sha1(os.urandom(64)).hexdigest()
     return session['_csrf_token']
 
-app.jinja_env.globals['csrf_token'] = generate_csrf_token()
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 # mod_wsgi needs this
 application = app
